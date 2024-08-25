f4372906207cae7acdcd2ce4109f603d87c79ec8
Be Graffic <contacto@begraffic.com>
2024-08-26 01:00:19 +0200

    cargar app

---
+++ src/components/BackgroundsView.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="relative w-screen h-screen">
+    <!-- Imagen de fondo mientras el video carga o si falla -->
+    <img
+      v-if="showImage"
+      :src="imageUrl"
+      class="object-cover w-screen h-screen"
+      alt="Weather image"
+    />
+
+    <!-- Video -->
+    <video
+      v-show="!showImage"
+      autoplay
+      muted
+      loop
+      :class="{ 'filter saturate-0': night }"
+      class="object-cover w-screen h-screen"
+      :src="videoUrl"
+      @canplay="onVideoCanPlay"
+      @error="onVideoError"
+    ></video>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<script lang="ts">
+export default {
+  name: 'BackgroundsView',
+
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      actual: {
+        id: 800,
+        name: 'Cielo despejado',
+        img: 'despejado.jpg',
+        video: 'despejado.mp4'
+      },
+      videoUrl: '' as any,
+      imageUrl: '' as any,
+      showImage: true,
+      night: false,
+      conditions: [
+        {
+          id: 200,
+          name: 'Tormenta',
+          img: 'tormenta.jpg',
+          video: 'tormenta.mp4'
+        },
+        {
+          id: 300,
+          name: 'Llovizna',
+          img: 'llovizna.jpg',
+          video: 'llovizna.mp4'
+        },
+        {
+          id: 500,
+          name: 'Lluvia',
+          img: 'lluvia.jpg',
+          video: 'lluvia.mp4'
+        },
+        {
+          id: 600,
+          name: 'Nieve',
+          img: 'nieve.jpg',
+          video: 'nieve.mp4'
+        },
+        {
+          id: 701,
+          name: 'Niebla',
+          img: 'niebla.jpg',
+          video: 'niebla.mp4'
+        },
+        {
+          id: 711,
+          name: 'Humo',
+          img: 'humo.jpg',
+          video: 'humo.mp4'
+        },
+        {
+          id: 721,
+          name: 'Neblina',
+          img: 'neblina.jpg',
+          video: 'neblina.mp4'
+        },
+        {
+          id: 731,
+          name: 'Polvo/Remolinos de arena',
+          img: 'arena.jpg',
+          video: 'arena.mp4'
+        },
+        {
+          id: 741,
+          name: 'Niebla espesa',
+          img: 'humo.jpg',
+          video: 'humo.mp4'
+        },
+        {
+          id: 751,
+          name: 'Arena',
+          img: 'arena.jpg',
+          video: 'arena.mp4'
+        },
+        {
+          id: 761,
+          name: 'Polvo',
+          img: 'humo.jpg',
+          video: 'humo.mp4'
+        },
+        {
+          id: 762,
+          name: 'Ceniza volcánica',
+          img: 'ceniza.jpg',
+          video: 'ceniza.mp4'
+        },
+        {
+          id: 771,
+          name: 'Chubascos',
+          img: 'chubascos.jpg',
+          video: 'chubascos.mp4'
+        },
+        {
+          id: 781,
+          name: 'Tornado',
+          img: 'tornado.jpg',
+          video: 'tornado.mp4'
+        },
+        {
+          id: 800,
+          name: 'Cielo despejado',
+          img: 'despejado.jpg',
+          video: 'despejado.mp4'
+        },
+        {
+          id: 801,
+          name: 'Pocas nubes',
+          img: 'pocon-ubes.jpg',
+          video: 'pocas-nubes.mp4'
+        },
+        {
+          id: 802,
+          name: 'Nubes dispersas',
+          img: 'nubes-dispersas.jpg',
+          video: 'nubes-dispersas.mp4'
+        },
+        {
+          id: 803,
+          name: 'Nubes rotas',
+          img: 'nubes-dispersas.jpg',
+          video: 'nubes-dispersas.mp4'
+        },
+        {
+          id: 804,
+          name: 'Nublado',
+          img: 'niebla.jpg',
+          video: 'niebla.mp4'
+        }
+      ]
+    }
+  },
+
+  mounted() {
+    this.updateVideoUrl(this.actual)
+  },
+
+  methods: {
+    updateVideoUrl(value: { img: any; video: any; }) {
+
+      if (value.img) {
+        this.imageUrl = new URL(`/src/assets/images/weather/${value.img}`, window.location.href)
+      }
+
+      if (value.video) {
+        this.videoUrl = new URL(`/src/assets/video/${value.video}`, window.location.href)
+      }
+    },
+    onVideoCanPlay() {
+      this.showImage = false // Oculta la imagen cuando el video está listo
+    },
+    onVideoError() {
+      this.showImage = true // Muestra la imagen si hay un error en la carga del video
+    }
+  },
+
+  watch: {
+    data() {
+      if (this.data) {
+        var aqui = this.conditions.find(
+          (condition) =>
+            condition.id >= this.data.current.weather[0].id &&
+            condition.id <= this.data.current.weather[0].id
+        )
+
+        if (aqui !== undefined) {
+          this.actual = aqui
+          this.updateVideoUrl(aqui)
+        }
+
+        /* detectar si es de noche */
+        const date = new Date(this.data.current.dt * 1000)
+        const hour = date.getHours()
+        this.night = hour < 6 || hour > 18
+
+        this.showImage = true // Muestra la imagen mientras se carga el nuevo video
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="mt-10 lg:mt-0">
+    <div class="relative mt-2 rounded-md shadow-sm lg:w-[500px] w-full">
+      <div
+        class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 lg:w-[500px] w-full"
+      >
+        <IconMagnify aria-hidden="true" class="h-5 w-5 text-white" />
+      </div>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        placeholder="Escribe tu ciudad"
+        className="block lg:w-[500px] w-full rounded-full border-0 py-3 pl-10 text-white ring-1 bg-white/10 ring-inset ring-gray-300 placeholder:text-white/50 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+        v-model="search"
+        @input="debouncedSearch"
+      />
+      <div
+        v-if="push"
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center justify-end pr-3 lg:w-[500px] w-full"
+      >
+        <IconArrowPath aria-hidden="true" class="h-5 w-5 text-white animate-spin" />
+      </div>
+    </div>
+
+    <div class="absolute mt-4" v-if="cities">
+      <div
+        class="bg-white/90 lg:w-[500px] w-full max-h-[300px] relative rounded-2xl overflow-y-auto"
+      >
+        <button
+          class="w-full px-10 pt-5 hover:bg-black/70 hover:text-white"
+          v-for="city in cities"
+          :key="city.key"
+          @click="fetchWeather(city)"
+        >
+          <p class="text-lg font-bold border-b-2 border-black/30 pb-4 text-left">
+            {{ city.name }},
+            <span class="font-normal text-base">{{ city.country }}</span>
+          </p>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import IconMagnify from './icons/IconMagnify.vue'
+import IconArrowPath from './icons/IconArrowPath.vue'
+
+// @ts-ignore
+import { getWeatherData } from '../controllers/weatherService'
+// @ts-ignore
+import { getAutocompleteText } from '../controllers/autoCompleteCity'
+</script>
+
+<script lang="ts">
+export default {
+  name: 'SearchBar',
+
+  props: [''],
+
+  data() {
+    return {
+      search: '' as any,
+      cities: [] as any,
+      weatherData: null as any,
+      push: false,
+      timeout: null as any
+    }
+  },
+
+  mounted() {},
+
+  computed: {},
+
+  components: {
+    IconMagnify,
+    IconArrowPath
+  },
+
+  methods: {
+    debouncedSearch() {
+      if (this.search.length < 3) {
+        this.push = false
+        this.cities = []
+        return
+      }
+
+      this.push = true
+
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        this.handleSearch()
+      }, 1000)
+    },
+
+    async handleSearch() {
+      this.cities = await getAutocompleteText(this.search)
+      console.table(this.cities)
+      this.push = false
+    },
+    async fetchWeather(city: any) {
+      this.push = true
+      this.weatherData = await getWeatherData(city)
+      this.saveRecord({
+        ...this.weatherData.current,
+        name: this.weatherData.name,
+        coord: {
+          lat: this.weatherData.lat,
+          lon: this.weatherData.lon
+        }
+      })
+      this.cities = []
+      this.search = ''
+      this.$emit('weather', this.weatherData)
+      this.push = false
+    },
+
+    saveRecord(record: { name: any; }) {
+      const data = localStorage.getItem('historicalData')
+      if (data) {
+        const historicalData = JSON.parse(data)
+
+        // Verificar si ya existe un record con el mismo nombre
+        const isDuplicate = historicalData.some((item: { name: any; }) => item.name === record.name)
+
+        if (!isDuplicate) {
+          historicalData.unshift(record)
+          localStorage.setItem('historicalData', JSON.stringify(historicalData))
+          this.$emit('historyCheck', true)
+        } else {
+          console.warn(`El nombre "${record.name}" ya existe en el historial.`)
+          // Puedes manejar el caso de duplicado según necesites, por ejemplo:
+          // this.$emit('duplicateRecord', record.name)
+        }
+      } else {
+        localStorage.setItem('historicalData', JSON.stringify([record]))
+        this.$emit('historyCheck', true)
+      }
+    }
+  },
+
+  watch: {},
+
+  created() {}
+}
+</script>
+
+<style scoped></style>
